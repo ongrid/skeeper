50bae7b227f51327f79ff2ae031b010fa8f9edcd
Kirill Varlamov <kirill@ongrid.pro>
2025-07-01 21:07:59 +0000

    style(keeper): use named selective import

--- src/SKeeper.sol
+++ src/SKeeper.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import "@openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeERC20, IERC20} from "@openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 import {AccessControl} from "@openzeppelin-contracts/access/AccessControl.sol";
 import {IERC1271} from "@openzeppelin-contracts/interfaces/IERC1271.sol";
 import {ECDSA} from "@openzeppelin-contracts/utils/cryptography/ECDSA.sol";
